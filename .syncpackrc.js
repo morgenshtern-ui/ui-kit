aec864095c427827b7fb5edbe6e45e01df1dede6
Валентин Степанов <teleskop150750@gmail.com>
2023-08-23 00:45:09 +0300

    chore: update

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -2,21 +2,23 @@
 
 /** @type {import("syncpack").RcFile} */
 const config = {
+  dependencyTypes: ['dev', 'prod'],
+  source: ['package.json', 'configs/*/package.json'],
   versionGroups: [
     {
       label: 'Внутренние пакеты конфигурации должны быть привязаны к "*" (это означает, что приемлема любая версия).',
       packages: ['**'],
       dependencies: [
-        "@teleskop-labs/eslint-config-js",
-        "@teleskop-labs/eslint-config-ts",
-        "@teleskop-labs/eslint-config-prettier-ts",
-        "@teleskop-labs/eslint-config-vue",
-        "@teleskop-labs/prettier-config",
-        "@teleskop-labs/stylelint-config-css",
-        "@teleskop-labs/tsconfig",
+        '@teleskop-labs/eslint-config-js',
+        '@teleskop-labs/eslint-config-ts',
+        '@teleskop-labs/eslint-config-prettier-ts',
+        '@teleskop-labs/eslint-config-vue',
+        '@teleskop-labs/prettier-config',
+        '@teleskop-labs/stylelint-config-css',
+        '@teleskop-labs/tsconfig',
       ],
       dependencyTypes: ['dev'],
-      pinVersion: '*',
+      pinVersion: 'workspace:*',
     },
   ],
 }
